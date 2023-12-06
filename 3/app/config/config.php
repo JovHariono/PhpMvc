e47a19178bcafe682dd1557606b8da95365c4b0e
email <myemail@mail.com>
2023-12-06 16:06:46 +0700

    belajar PHP MVC

---
+++ 3/app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+define('BASEURL', 'http://localhost:8080/phpmvc/3/public');
+
+//DB
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "phpmvc");
---
+++ 5/public/js/script.js
@@ -0,0 +1,31 @@
+$(function(){
+    $('.tombolTambahData').on("click", function(){
+        $("#formModalLabel").html("Tambah Data Mahasiswa")
+        $(".modal-footer button[type=submit]").html('Submit')
+
+    })
+
+    $('.tampilModalUbah').on('click', function(){
+        $("#formModalLabel").html("Ubah Data Mahasiswa")
+        $(".modal-footer button[type=submit]").html('Ubah Data')
+        $(".modal-body form").attr("action", "http://localhost:8080/phpmvc/5/public/mahasiswa/ubah")
+
+        const id= $(this).data('id');
+        
+        $.ajax({
+            url: 'http://localhost:8080/phpmvc/5/public/mahasiswa/getubah',
+            data: {id : id},
+            method: "post",
+            dataType: 'json',
+            success : function(data){
+                $('#id').val(data.id)
+                $('#nama').val(data.nama)
+                $('#nrp').val(data.nrp)
+                $('#email').val(data.email)
+                $('#jurusan').val(data.jurusan)
+            }
+        })
+
+    })
+
+})
